bff2e975ab6cec2f62efdb3443d99d43ca781701
FyuroTEX <yurilev4@gmail.com>
2020-02-06 03:54:44 +0200

    change README

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+    <div>
+        <h1>My ToDo List</h1>
+        <input type="text" placeholder='search' />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
