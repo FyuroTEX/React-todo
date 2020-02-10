c4649b44355a4ba5b056b26df7b8fffe5213c978
FyuroTEX <yurilev4@gmail.com>
2020-02-10 03:21:32 +0200

    use .bind()

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -5,6 +5,10 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  onLabelClick() {
+    console.log(`Done: ${this.props.label}`);
+  }
+
   render() {
 
     const { label, important = false } = this.props;
@@ -15,10 +19,11 @@ export default class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
+      <div className="todo-list-item">
         <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={this.onLabelClick.bind(this)}>
           {label}
         </span>
 
@@ -31,7 +36,7 @@ export default class TodoListItem extends Component {
           className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o" />
         </button>
-      </span>
+      </div>
     );
   }
 
