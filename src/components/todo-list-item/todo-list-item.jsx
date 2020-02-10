c2641e811d85b503b4c19ec1af2c4b2f98b5fe58
FyuroTEX <yurilev4@gmail.com>
2020-02-10 21:32:40 +0200

    toggle class

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -7,13 +7,19 @@ export default class TodoListItem extends Component {
 
   constructor() {
     super();
+    this.state = {
+      done: false
+    };
     // this.onLabelClick = () => {
     //   console.log(`Done: ${this.props.label}`);
     // };
     this.onLabelClick = this.onLabelClick.bind(this); //Delete if use first variant
   };
-  onLabelClick() {
-    console.log(`Done: ${this.props.label}`); //Delete if use first variant
+  onLabelClick() {  //Delete if use first variant
+    this.setState({
+      done: !this.state.done
+    });
+    // console.log(this.state.done)
   };
   // onLabelClick = () => {
   //   console.log(`Done: ${this.props.label}`); //Use arrow function withouts constructirs and bindings
@@ -23,13 +29,19 @@ export default class TodoListItem extends Component {
 
     const { label, important = false } = this.props;
 
+    const { done } = this.state;
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += ' done';
+    }
+
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
 
     return (
-      <div className="todo-list-item">
+      <div className={classNames}>
         <span
           className="todo-list-item-label"
           style={style}
